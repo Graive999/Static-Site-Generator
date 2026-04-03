68c5585e17ea37784fcef5f308166f4e6ba77822
graive999 <tdh679@gmail.com>
2026-04-03 16:56:50 -0400

    TextNode to HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
-
+from textnode  import TextNode, TextType
+from textnode import text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -22,6 +22,12 @@ class TestTextNode(unittest.TestCase):
     def test_eq_false(self):
         node = TextNode("This is a text node", TextType.TEXT)
         self.assertIsNone(node.url)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
